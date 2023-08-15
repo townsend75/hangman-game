89c9981b4aec614d3a26a0e2891f63df40588156
townsend75 <>
2023-08-15 09:47:52 +0000

    initial commit plus user welcome and randomized word function

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+name = input("Please enter your name here: \n")
+print(f"Welcome to Hangman, {name}!")
+print("Guess the secret word, one letter at a time")
+print("But beware: after 10 incorrect guesses, you lose!")
+
+words = ["kettle", "caterpiller", "antelope", "computer", "magnet", "telegraph", "telescope", "butcher", "tradition", "envelope", "trivial", "extraterrestrial", "sausage"]
+
+
+def getRandomWord():
+    word = random.choice(words)
+    guessWord = "- " * len(word)
+    print(guessWord)
+
+
+getRandomWord()
