7b8719d226ce6edb5ebe3f53827f56cb2541169e
townsend75 <>
2023-08-16 09:05:59 +0000

    Get word to be recognised in game function

--- run.py
+++ run.py
@@ -7,36 +7,82 @@ import random
 name = input("Please enter your name here: \n")
 print(f"Welcome to Hangman, {name}!")
 print("Guess the secret word, one letter at a time")
-print("But beware: after 10 incorrect guesses, you lose!")
+print("But beware: after 10 incorrect guesses, you lose!\n")
 
 words = ["kettle", "caterpiller", "antelope", "computer", "magnet",
          "telegraph", "telescope", "butcher", "tradition", "envelope", 
          "trivial", "extraterrestrial", "sausage"]
 
+lettersGuessed = []
+word = ""
+guess = ""
 
 def getRandomWord():
     """Retrieve a random word from the word list and 
     print hyphens corresponding to number of letters in word"""
 
     word = random.choice(words)
-    guessWord = "- " * len(word)
+    
+    #guessWord = "- " * len(word)
     print("Guess the word...")
-    print(guessWord)
+    print(word)
+    
+    
+    return word
+    
+    
 
 def userGuess():
     """Evaluate user guess"""
-    userInput = ""
+    guess = ""
 
     while True:
 
-        userInput = input("Enter a single letter\n").lower()
+        guess = input("Enter a single letter\n").lower()
 
-        if userInput.isalpha() and len(userInput) == 1:
-            print(userInput)
+        if guess.isalpha() and len(guess) == 1:
+            print(guess)
+
+        
             break
         else:
-            print("You may only enter a single letter of the alphabet!")
-            print("Please try again")
+                print("You may only enter a single letter of the alphabet!")
+                print("Please try again")
+
+    #print(f"You guessed {guess}")
+    return guess 
+    
+
+           
+
+def game(word): 
+
+    letterGuessed = []
+    wordsGuessed = []
+    currentGame = "- " * len(word)
+    guessed = False
+    tries = 10
+
+    print(currentGame)
+    print(guess)
+    print(word)
+
+    if guess in word:
+        print(word)
+
+    else:
+        print("No way Jose")   
+    
+    
+    
+            
+            
+
+
+
+
+
+
 
 
 
@@ -48,5 +94,6 @@ def userGuess():
 
 
 
-getRandomWord()
+word = getRandomWord()
 userGuess()
+game(word)
